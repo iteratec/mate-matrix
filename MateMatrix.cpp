7b496394596ac40ef771b6522225fe5f85477089
Stefan Burnicki <stefan.burnicki@iteratec.de>
2018-08-31 16:50:00 +0200

    Mehr Funktionen

--- MateMatrix.cpp
+++ MateMatrix.cpp
@@ -11,8 +11,29 @@
 int dataPin = 8;    // DIN pin of MAX7219 module
 int chipSelectPin = 9;    // CS pin of MAX7219 module
 int clockPin = 10;  // CLK pin of MAX7219 module
+unsigned long framecount = 0;
+unsigned long time = millis();
 
 LedControl lc = LedControl(dataPin, clockPin, chipSelectPin, 1);
+
+
+byte currentMatrix[] = {
+  B00000000,
+  B00000000,
+  B00000000,
+  B00000000,
+  B00000000,
+  B00000000,
+  B00000000,
+  B00000000
+};
+
+byte reverse(byte b) {
+   b = (b & 0xF0) >> 4 | (b & 0x0F) << 4;
+   b = (b & 0xCC) >> 2 | (b & 0x33) << 2;
+   b = (b & 0xAA) >> 1 | (b & 0x55) << 1;
+   return b;
+}
 
 void initialisiereMatrix() {
   int ledBrightness = 3; // dot matix intensity 0-15
@@ -21,6 +42,10 @@ void initialisiereMatrix() {
     lc.setScanLimit(index, 7); // use all digits
     lc.setIntensity(index, ledBrightness); // set brightness
   }
+
+  for(int i = 0;i<8;i++) {
+    currentMatrix[i] = B00000000;
+  }
 }
 
 void schalteMatrixAus(){
@@ -28,12 +53,51 @@ void schalteMatrixAus(){
   for(int index=0; index < lc.getDeviceCount(); index++){
     lc.clearDisplay(index);
   }
+
+  for(int i = 0;i<8;i++) {
+    currentMatrix[i] = B00000000;
+  }
+}
+
+void zeigeGespeichertesBild(){
+  Serial.println("displaying frame");
+  for(int i = 0; i<8; i++){
+    lc.setColumn(0, i, reverse(currentMatrix[i]));
+  }
+}
+
+void speichereBild(byte columns[]){
+  Serial.println("displaying frame");
+  for(int i = 0; i<8; i++){
+    currentMatrix[i] = columns[i]|currentMatrix[i];
+    lc.setColumn(0, i, reverse(currentMatrix[i]));
+  }
+}
+
+void messeFPS(){
+  framecount++;
+  unsigned long now = millis();
+  int diff = now - time;
+  if(diff > 10000){
+    int fps = framecount / (diff / 1000);
+    Serial.print("Frames per Second: ");
+    Serial.println(fps);
+    time = now;
+    framecount = 0;
+  }
 }
 
 void zeigeBild(byte columns[]){
   Serial.println("displaying frame");
+  messeFPS();
   for(int i = 0; i<8; i++){
-    lc.setColumn(0, i, columns[i]);
+    lc.setColumn(0, i, reverse(columns[i]));
   }
 }
 
+char leseZeichen() {
+  if (Serial.available()) {
+   return Serial.read();
+  }
+  return 0;
+}
